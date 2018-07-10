fd4e15d17e519838bf404b909b278a427c9d8719
ryuuji3 <mori.ryuuji3@gmail.com>
2018-07-09 20:36:54 -0400

    Create login page

---
+++ src/components/login.form.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-form @submit="onSubmit">
+      <b-form-group
+        label="Username:"
+        label-for="username"
+      >
+        <b-form-input 
+          id="username"
+          type="email"
+          required
+          placeholder="email@example.com"
+          v-model.trim="username"
+        />
+      </b-form-group>
+      <b-form-group
+        label="Password:"
+        label-for="password"
+      >
+        <b-form-input
+          id="password"
+          type="password"
+          required
+          placeholder="password"
+          v-model.trim="password"
+          />
+      </b-form-group>
+      <b-form-group>
+        <b-form-checkbox-group>
+          <b-form-checkbox
+          id="remember"
+          v-model="remember"
+          >
+            Remember Me
+          </b-form-checkbox>
+        </b-form-checkbox-group>
+        <b-button type="submit" variant="primary">Login</b-button>
+      </b-form-group>
+  </b-form>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  name: 'LoginForm',
+  computed: {},
+  data: () => ({
+    username: '',
+    password: '',
+    remember: false,
+  }),
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+    },
+  },
+};
+</script>
---
+++ src/views/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-container class="fluid centered">
+    <h2>Login</h2>
+    <LoginForm/>
+  </b-container>
+</template>
+
+<script>
+import LoginForm from "@/components/login.form.vue"
+
+export default {
+  name: "login",
+  components: {
+    LoginForm
+  }
+};
+</script>
+
+<style>
+.centered {
+  margin: 0 auto;
+  width: 40%;
+}
+</style>
